d280b2a1f9e0f062e938df90b33687382d5e15b0
Guerrero <guerrerov.robertocarlos00@gmail.com>
2025-06-12 22:07:48 -0600

    diseño de index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="container bg-white mt-4" style="border-radius: 10px; padding: 20px; box-shadow: 0 0 40px #5E0022">
+    <div class="row">
+        <div class="col-12 img-fluid d-flex justify-content-center user-select-none">
+            <img src="{{ asset('img/logo-institucional-utec.jpeg') }}" alt="Logo" class="img-fluid user-select-none" style="max-width: 100%; height: auto;">
         </div>
     </div>
+    <h1 class="display-5 text-center">Sistema de Administración de Graduados de Carreras Técnicas</h1>
 </div>
 @endsection
